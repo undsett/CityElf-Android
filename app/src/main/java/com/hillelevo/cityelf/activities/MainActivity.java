15eed55a63247d62c24a90ba4522875d0188a501
Sergii Litvinenko <litvinenko.sergii@gmail.com>
2017-05-20 16:48:04 +0300

    Add .travis.yml and format code

--- app/src/main/java/com/hillelevo/cityelf/activities/MainActivity.java
+++ app/src/main/java/com/hillelevo/cityelf/activities/MainActivity.java
@@ -7,9 +7,9 @@ import com.hillelevo.cityelf.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
 }
--- app/src/main/java/com/hillelevo/cityelf/activities/MapActivity.java
+++ app/src/main/java/com/hillelevo/cityelf/activities/MapActivity.java
@@ -1,8 +1,6 @@
 package com.hillelevo.cityelf.activities;
 
-/**
- * Created by falcon on 13.05.17.
- */
 
 public class MapActivity {
+
 }

554536cf210df75f4a6b34e28b444c2f9e72fbdb
Aleksandr Timoshenko <aleksandrstim@gmail.com>
2017-06-02 21:18:59 +0300

    S2T17: AsyncTask creation for client requests to server

--- app/src/main/java/com/hillelevo/cityelf/activities/MainActivity.java
+++ app/src/main/java/com/hillelevo/cityelf/activities/MainActivity.java
@@ -1,26 +1,34 @@
 package com.hillelevo.cityelf.activities;
 
-import static com.hillelevo.cityelf.Constants.TAG;
-
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.hillelevo.cityelf.Constants;
 import com.hillelevo.cityelf.Constants.Actions;
 import com.hillelevo.cityelf.Constants.Params;
 import com.hillelevo.cityelf.R;
+import com.hillelevo.cityelf.webutils.JsonMassageTask;
+
+import static com.hillelevo.cityelf.Constants.TAG;
 
 public class MainActivity extends AppCompatActivity {
 
+
   private Button btnTest;
+  private Button btnJsonTest;
+  private static String result;
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -36,12 +44,23 @@ public class MainActivity extends AppCompatActivity {
       }
     });
 
+    btnJsonTest = (Button) findViewById(R.id.btnJsonTest);
+    btnJsonTest.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        new JsonMassageTask().execute(Constants.TEST_URL);
+        Toast toast = Toast.makeText(MainActivity.this, result, Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+        toast.show();
+      }
+    });
+
     // Create LocalBroadcastManager and register it to all actions;
     LocalBroadcastManager messageBroadcastManager = LocalBroadcastManager.getInstance(this);
     messageBroadcastManager.registerReceiver(MessageReceiver,
-        new IntentFilter(Actions.BROADCAST_ACTION_FIREBASE_TOKEN));
+            new IntentFilter(Actions.BROADCAST_ACTION_FIREBASE_TOKEN));
     messageBroadcastManager.registerReceiver(MessageReceiver,
-        new IntentFilter(Actions.BROADCAST_ACTION_FIREBASE_MESSAGE));
+            new IntentFilter(Actions.BROADCAST_ACTION_FIREBASE_MESSAGE));
   }
 
   /**
@@ -59,4 +78,7 @@ public class MainActivity extends AppCompatActivity {
     }
   };
 
+  public static void reciveResult(String output){
+    result = output;
+  }
 }

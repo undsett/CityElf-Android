cfbccfbdabcaf38c95b291da518df9b1a8250b8a
MikhailovOlegM <mikhailov.oleg.m@gmail.com>
2017-08-15 00:36:57 +0300

    Splash Activity background

--- app/src/main/java/com/hillelevo/cityelf/activities/SplashActivity.java
+++ app/src/main/java/com/hillelevo/cityelf/activities/SplashActivity.java
@@ -1,14 +1,19 @@
 package com.hillelevo.cityelf.activities;
 
+import com.hillelevo.cityelf.R;
+
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+    setTheme(R.style.SplashTheme);
 
     Intent intent = new Intent(this, MainActivity.class);
     startActivity(intent);
